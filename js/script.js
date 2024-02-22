a19fbbdcca3e59f0069a5014c9a80da34ea1e1f9
albertoferri <falberto99@gmail.com>
2024-02-22 12:22:48 +0100

    aggiunte immagini

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // ******** VUE ********
-const { createApp } = Vue;
-createApp({
-    data() {
-        return {
+// const { createApp } = Vue;
+// createApp({
+//     data() {
+//         return {
             
-        }
-    }
-}).mount("#app");
+//         }
+//     }
+// }).mount("#app");
 // ******** VUE ********
